21312e9fff0449d8fe157c1e8c6077aa95bc3343
Peter Parkanyi <peter@redsift.io>
2020-03-19 10:53:12 +0000

    Regenerate site with new theme

--- en.search.min.70910593e77336d697e5d3d4bc993888220cdc9015258355843e3016ab5e3ca8.js
+++ en.search.min.70910593e77336d697e5d3d4bc993888220cdc9015258355843e3016ab5e3ca8.js
@@ -2,7 +2,7 @@
 const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
 input.focus();e.preventDefault();}
 function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
-function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.ea22af06ff4adec7081bce6243d3e2536403e915e94b47a57bd242848c668bfe.js',function(){input.required=false;search();});}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.0977e21452e799d2971f99906653f4acef818856306c486099540749c74b5e49.js',function(){input.required=false;search();});}
 function search(){while(results.firstChild){results.removeChild(results.firstChild);}
 if(!input.value){return;}
 const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});}
