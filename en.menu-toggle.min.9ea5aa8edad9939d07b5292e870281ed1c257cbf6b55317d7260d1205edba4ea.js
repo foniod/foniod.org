27116e384f467ddfd1eb622088c8d33a028c8765
Peter Parkanyi <peter@redsift.io>
2020-03-19 14:52:10 +0000

    Update with new mobile menu

---
+++ en.menu-toggle.min.9ea5aa8edad9939d07b5292e870281ed1c257cbf6b55317d7260d1205edba4ea.js
@@ -0,0 +1,2 @@
+(function(){const menuButton=document.querySelector(".nav-opener");const toggleMenu=()=>{document.querySelector(".sidebar").classList.toggle('mobile-open');}
+menuButton.onclick=toggleMenu;})();
