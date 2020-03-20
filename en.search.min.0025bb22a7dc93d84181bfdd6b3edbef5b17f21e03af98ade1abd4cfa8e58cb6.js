1ebe2bc1a9d1c67840c0e588dfcd81d7c7f88ffe
Peter Parkanyi <peter@redsift.io>
2020-03-20 15:50:36 +0000

    Render changes

---
+++ en.search.min.0025bb22a7dc93d84181bfdd6b3edbef5b17f21e03af98ade1abd4cfa8e58cb6.js
@@ -0,0 +1,10 @@
+'use strict';(function(){const input=document.querySelector('#book-search-input');const results=document.querySelector('#book-search-results');const mainMenu=document.querySelector('.sidebar .nav-holder > ul');input.addEventListener('focus',init);input.addEventListener('blur',restore);input.addEventListener('keyup',search);document.addEventListener('keypress',focusSearchFieldOnKeyPress);function focusSearchFieldOnKeyPress(e){if(input===document.activeElement){return;}
+const characterPressed=String.fromCharCode(e.charCode);if(!isHotkey(characterPressed)){return;}
+input.focus();e.preventDefault();}
+function isHotkey(character){const dataHotkeys=input.getAttribute('data-hotkeys')||'';return dataHotkeys.indexOf(character)>=0;}
+function init(){input.removeEventListener('focus',init);input.required=true;loadScript('/flexsearch.min.js');loadScript('/en.search-data.min.0977e21452e799d2971f99906653f4acef818856306c486099540749c74b5e49.js',function(){input.required=false;search();});}
+function search(){while(results.firstChild){results.removeChild(results.firstChild);}
+if(!input.value){return;}
+const searchHits=window.bookSearchIndex.search(input.value,10);searchHits.forEach(function(page){const li=document.createElement('li'),a=li.appendChild(document.createElement('a'));a.href=page.href;a.textContent=page.title;results.appendChild(li);});if(searchHits.length&&input.value.length){mainMenu.classList.add('hide');}else{const noResults=document.createElement('li');noResults.textContent='No results found';noResults.classList.add('no-results');results.appendChild(noResults);}}
+function restore(){const searchHits=window.bookSearchIndex.search(input.value,10);if(!searchHits.length&&!input.value.length){mainMenu.classList.remove('hide');while(results.firstChild){results.removeChild(results.firstChild);}}}
+function loadScript(src,callback){const script=document.createElement('script');script.defer=true;script.async=false;script.src=src;script.onload=callback;document.head.appendChild(script);}})();
