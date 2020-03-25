65b466319d5fb6bebc00f3b5ca0fee41b7fd2bf4
Peter Parkanyi <peter@redsift.io>
2020-03-25 17:51:48 +0000

    Render site

--- en.menu-toggle.min.9ea5aa8edad9939d07b5292e870281ed1c257cbf6b55317d7260d1205edba4ea.js
+++
@@ -1,2 +0,0 @@
-(function(){const menuButton=document.querySelector(".nav-opener");const toggleMenu=()=>{document.querySelector(".sidebar").classList.toggle('mobile-open');}
-menuButton.onclick=toggleMenu;})();
