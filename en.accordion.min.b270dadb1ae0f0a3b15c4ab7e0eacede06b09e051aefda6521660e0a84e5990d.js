da26b4a2c7ce402710984ab417d8d047bb418ad9
Peter Parkanyi <peter@redsift.io>
2020-03-31 15:50:28 +0100

    Update landing gif

--- en.accordion.min.b270dadb1ae0f0a3b15c4ab7e0eacede06b09e051aefda6521660e0a84e5990d.js
+++
@@ -1,6 +0,0 @@
-(function(){const tabButtons=document.querySelectorAll(".tabset li a");const tabs=document.querySelectorAll(".tab-info");let index=0;let hashIndex=null;if(tabButtons.length!==tabs.length){console.warn("Add the same number of tabs as tab buttons you silly billy!!!");}
-const init=()=>{const tabIds=Array.from(tabs).map(i=>i.id);const hash=window.location.hash;if(hash&&tabIds.includes(hash.substring(1))){Array.from(tabButtons).find(i=>i.hash===hash).classList.add("active");Array.from(tabs).find(i=>i.id===hash.substring(1)).classList.add("active");hashIndex=Array.from(tabButtons).findIndex(i=>i.hash===hash);}else{[tabButtons,tabs].map(i=>i[0].classList.add("active"));}}
-const setActive=(prevIndex,index)=>{if(hashIndex){tabs[hashIndex].classList.remove("active");tabButtons[hashIndex].classList.remove("active");}
-[tabButtons,tabs].map(i=>{i[prevIndex].classList.remove("active");i[index].classList.add("active");});}
-const selectTab=btnIdx=>{const prevIndex=index;if((prevIndex||hashIndex)!==btnIdx){index=btnIdx;setActive(prevIndex,index);}}
-init();Array.from(tabButtons).map((val,idx)=>val.onclick=()=>selectTab(idx));})();
---
+++ en.accordion.min.fbed1f9cac4430dd9d829b8b700f8af1c71f1c1b6b8b7b24ad6f013f44872cfe.js
@@ -0,0 +1,7 @@
+(function(){const selectAnchors=()=>{let selector=".tabset li a";if(window.innerWidth<768){selector=".opener";}else{selector=".tabset li a";}
+return Array.from(document.querySelectorAll(selector));};let tabAnchors=selectAnchors();const tabs=Array.from(document.querySelectorAll(".tab-info"));let index=0;let hashIndex=null;if(tabAnchors.length!==tabs.length){console.warn("Add the same number of tabs as tab buttons you silly billy!!!");}
+const init=()=>{const tabIds=tabs.map(i=>i.id);const hash=window.location.hash;if(hash&&tabIds.includes(hash.substring(1))){tabAnchors.find(i=>i.hash===hash).classList.add("active");tabs.find(i=>i.id===hash.substring(1)).classList.add("active");hashIndex=tabAnchors.findIndex(i=>i.hash===hash);}else{[tabAnchors,tabs].map(i=>i[0].classList.add("active"));}}
+const setActive=(prevIndex,index)=>{if(hashIndex){tabs[hashIndex].classList.remove("active");tabAnchors[hashIndex].classList.remove("active");}
+[tabAnchors,tabs].map(i=>{i[prevIndex].classList.remove("active");i[index].classList.add("active");});}
+const selectTab=btnIdx=>{const prevIndex=index;if((prevIndex||hashIndex)!==btnIdx){index=btnIdx;setActive(prevIndex,index);}}
+init();tabAnchors.map((val,idx)=>val.onclick=()=>selectTab(idx));window.addEventListener('resize',()=>{tabAnchors.map(i=>i.removeEventListener("onclick",selectTab,false));tabAnchors=selectAnchors();tabAnchors.map((val,idx)=>val.onclick=()=>selectTab(idx));})})();
